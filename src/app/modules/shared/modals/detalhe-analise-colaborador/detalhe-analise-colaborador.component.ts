4f04251ac734c5eb9e5b4c4c18c76a1f83ba796d
artcousan <artcousan@gmail.com>
2024-08-26 21:52:49 -0300

    .

--- src/app/modules/shared/modals/detalhe-analise-colaborador/detalhe-analise-colaborador.component.ts
+++ src/app/modules/shared/modals/detalhe-analise-colaborador/detalhe-analise-colaborador.component.ts
@@ -1,15 +1,16 @@
-import { Component, EventEmitter, Input, Output, signal } from '@angular/core';
+import { Component, EventEmitter, Input, Output, OnInit, signal } from '@angular/core';
 import { AnaliseColaborador } from '../../../../core/dto/analise-colaborador';
 import { faCheck, faAngleLeft } from '@fortawesome/free-solid-svg-icons';
 import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
-import { Cargo, Colaborador, Departamento, EstadoCivil, Faculdade, FaixaSalarial, Formacao, Genero, NivelEscolaridade, Setor } from '../../../../core/dto/colaborador';
+import { Cargo, Departamento, EstadoCivil, Faculdade, FaixaSalarial, Formacao, Genero, NivelEscolaridade, Setor } from '../../../../core/dto/colaborador';
+import { SelecoesService } from '../../../../service/selecoes.service';
 
 @Component({
   selector: 'app-detalhe-analise-colaborador',
   templateUrl: './detalhe-analise-colaborador.component.html',
   styleUrl: './detalhe-analise-colaborador.component.scss'
 })
-export class DetalheAnaliseColaboradorComponent {
+export class DetalheAnaliseColaboradorComponent implements OnInit {
 
   @Input()
   analiseColaborador!: AnaliseColaborador;
@@ -23,126 +24,42 @@ export class DetalheAnaliseColaboradorComponent {
   readonly panelCargo = signal(false);
   readonly panelCarreira = signal(false);
 
-  generos: Genero[] = [
-    { id: 1, descricao: 'Masculino' },
-    { id: 2, descricao: 'Feminino' },
-    { id: 3, descricao: 'Outros' }
-  ];
-  
-  estadosCivis: EstadoCivil[] = [
-    { id: 1, descricao: 'Solteiro' },
-    { id: 2, descricao: 'Casado' },
-    { id: 3, descricao: 'Viuvo' },
-    { id: 4, descricao: 'Divorciado' },
-    { id: 5, descricao: 'Solteira' },
-    { id: 6, descricao: 'Casada' },
-    { id: 7, descricao: 'Viuva' },
-    { id: 8, descricao: 'Divorciada' }
-  ];
-  
-  niveisEscolaridade: NivelEscolaridade[] = [
-    { id: 1, descricao: 'Ensino Fundamental - Completo' },
-    { id: 2, descricao: 'Ensino Fundamental - Incompleto' },
-    { id: 3, descricao: 'Ensino Médio - Completo' },
-    { id: 4, descricao: 'Ensino Médio - Incompleto' },
-    { id: 5, descricao: 'Ensino Superior - Completo' },
-    { id: 6, descricao: 'Ensino Superior - Incompleto' }
-  ];
-  
-  faculdades: Faculdade[] = [
-    { id: 1, nome: 'USP' },
-    { id: 2, nome: 'PUC-SP' },
-    { id: 3, nome: 'FGV' }
-  ];
-  
-  formacoes: Formacao[] = [
-    { id: 1, descricao: 'Ciência da Computação' },
-    { id: 2, descricao: 'Engenharia de Software' },
-    { id: 3, descricao: 'Administração' }
-  ];
-  
-  departamentos: Departamento[] = [
-    { id: 1, nome: 'TI' },
-    { id: 2, nome: 'Financeiro' },
-    { id: 3, nome: 'RH' }
-  ];
-  
-  setores: Setor[] = [
-    { id: 1, nome: 'Desenvolvimento' },
-    { id: 2, nome: 'Contabilidade' },
-    { id: 3, nome: 'Recrutamento' }
-  ];
-  
-  faixasSalariais: FaixaSalarial[] = [
-    { id: 1, descricao: 'R$ 1.000 - R$ 1.500' },
-    { id: 2, descricao: 'R$ 1.501 - R$ 2.000' },
-    { id: 3, descricao: 'R$ 2.001 - R$ 2.500' },
-    { id: 4, descricao: 'R$ 2.501 - R$ 3.000' },
-    { id: 5, descricao: 'R$ 3.001 - R$ 3.500' },
-    { id: 6, descricao: 'R$ 3.501 - R$ 4.000' },
-    { id: 7, descricao: 'R$ 4.001 - R$ 4.500' },
-    { id: 8, descricao: 'R$ 4.501 - R$ 5.000' },
-    { id: 9, descricao: 'R$ 5.001 - R$ 5.500' },
-    { id: 10, descricao: 'R$ 5.501 - R$ 6.000' },
-    { id: 11, descricao: 'R$ 6.001 - R$ 6.500' },
-    { id: 12, descricao: 'R$ 6.501 - R$ 7.000' },
-    { id: 13, descricao: 'R$ 7.001 - R$ 7.500' },
-    { id: 14, descricao: 'R$ 7.501 - R$ 8.000' },
-    { id: 15, descricao: 'R$ 8.001 - R$ 8.500' },
-    { id: 16, descricao: 'R$ 8.501 - R$ 9.000' },
-    { id: 17, descricao: 'R$ 9.001 - R$ 9.500' },
-    { id: 18, descricao: 'R$ 9.501 - R$ 10.000' },
-    { id: 19, descricao: 'R$ 10.001 - R$ 10.500' },
-    { id: 20, descricao: 'R$ 10.501 - R$ 11.000' },
-    { id: 21, descricao: 'R$ 11.001 - R$ 11.500' },
-    { id: 22, descricao: 'R$ 11.501 - R$ 12.000' },
-    { id: 23, descricao: 'R$ 12.001 - R$ 12.500' },
-    { id: 24, descricao: 'R$ 12.501 - R$ 13.000' },
-    { id: 25, descricao: 'R$ 13.001 - R$ 13.500' },
-    { id: 26, descricao: 'R$ 13.501 - R$ 14.000' },
-    { id: 27, descricao: 'R$ 14.001 - R$ 14.500' },
-    { id: 28, descricao: 'R$ 14.501 - R$ 15.000' }
-  ];
-  
-  cargos: Cargo[] = [
-    { id: 1, nome: 'Desenvolvedor' },
-    { id: 2, nome: 'Analista Financeiro' },
-    { id: 3, nome: 'Gerente de TI' }
-  ];  
+  generos: Genero[] = [];
+  estadosCivis: EstadoCivil[] = [];
+  niveisEscolaridade: NivelEscolaridade[] = [];
+  faculdades: Faculdade[] = [];
+  formacoes: Formacao[] = [];
+  departamentos: Departamento[] = [];
+  setores: Setor[] = [];
+  faixasSalariais: FaixaSalarial[] = [];
+  cargos: Cargo[] = [];
 
   estados: string[] = [
-    'AC',  // Acre
-    'AL',  // Alagoas
-    'AP',  // Amapá
-    'AM',  // Amazonas
-    'BA',  // Bahia
-    'CE',  // Ceará
-    'DF',  // Distrito Federal
-    'ES',  // Espírito Santo
-    'GO',  // Goiás
-    'MA',  // Maranhão
-    'MT',  // Mato Grosso
-    'MS',  // Mato Grosso do Sul
-    'MG',  // Minas Gerais
-    'PA',  // Pará
-    'PB',  // Paraíba
-    'PR',  // Paraná
-    'PE',  // Pernambuco
-    'PI',  // Piauí
-    'RJ',  // Rio de Janeiro
-    'RN',  // Rio Grande do Norte
-    'RS',  // Rio Grande do Sul
-    'RO',  // Rondônia
-    'RR',  // Roraima
-    'SC',  // Santa Catarina
-    'SP',  // São Paulo
-    'SE',  // Sergipe
-    'TO'   // Tocantins
+    'AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO'
   ];
 
-  constructor(library: FaIconLibrary){
+  constructor(
+    library: FaIconLibrary,
+    private listasService: SelecoesService
+  ) {
     library.addIcons(faCheck);
     library.addIcons(faAngleLeft);
+  }
+
+  ngOnInit(): void {
+    this.carregarListas();
+  }
+
+  carregarListas(): void {
+    this.listasService.listarGeneros().subscribe(data => this.generos = data);
+    this.listasService.listarEstadosCivis().subscribe(data => this.estadosCivis = data);
+    this.listasService.listarNiveisEscolaridade().subscribe(data => this.niveisEscolaridade = data);
+    this.listasService.listarFaculdades().subscribe(data => this.faculdades = data);
+    this.listasService.listarFormacoes().subscribe(data => this.formacoes = data);
+    this.listasService.listarDepartamentos().subscribe(data => this.departamentos = data);
+    this.listasService.listarSetores().subscribe(data => this.setores = data);
+    this.listasService.listarFaixasSalariais().subscribe(data => this.faixasSalariais = data);
+    this.listasService.listarCargos().subscribe(data => this.cargos = data);
   }
 
   onVoltar() {
@@ -162,5 +79,4 @@ export class DetalheAnaliseColaboradorComponent {
       return 'red';
     }
   }
-
 }
---
+++ src/app/service/selecoes.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Cargo, Departamento, EstadoCivil, Faculdade, FaixaSalarial, Formacao, Genero, NivelEscolaridade, Setor } from '../core/dto/colaborador';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SelecoesService {
+  private apiUrl = 'http://localhost:5000/api';  // Base URL para as APIs
+
+  constructor(private http: HttpClient) {}
+
+  // Listar Gêneros
+  listarGeneros(): Observable<Genero[]> {
+    return this.http.get<Genero[]>(`${this.apiUrl}/generos`);
+  }
+
+  // Listar Estados Civis
+  listarEstadosCivis(): Observable<EstadoCivil[]> {
+    return this.http.get<EstadoCivil[]>(`${this.apiUrl}/estados-civis`);
+  }
+
+  // Listar Níveis de Escolaridade
+  listarNiveisEscolaridade(): Observable<NivelEscolaridade[]> {
+    return this.http.get<NivelEscolaridade[]>(`${this.apiUrl}/niveis-escolaridade`);
+  }
+
+  // Listar Faculdades
+  listarFaculdades(): Observable<Faculdade[]> {
+    return this.http.get<Faculdade[]>(`${this.apiUrl}/faculdades`);
+  }
+
+  // Listar Formações
+  listarFormacoes(): Observable<Formacao[]> {
+    return this.http.get<Formacao[]>(`${this.apiUrl}/formacoes`);
+  }
+
+  // Listar Departamentos
+  listarDepartamentos(): Observable<Departamento[]> {
+    return this.http.get<Departamento[]>(`${this.apiUrl}/departamentos`);
+  }
+
+  // Listar Setores
+  listarSetores(): Observable<Setor[]> {
+    return this.http.get<Setor[]>(`${this.apiUrl}/setores`);
+  }
+
+  listarFaixasSalariais(): Observable<FaixaSalarial[]> {
+    return this.http.get<FaixaSalarial[]>(`${this.apiUrl}/faixas-salariais`);
+  }
+
+  listarCargos(): Observable<Cargo[]> {
+    return this.http.get<Cargo[]>(`${this.apiUrl}/cargos`);
+  }
+
+
+}
